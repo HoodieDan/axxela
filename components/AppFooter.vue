8177608707e054b3a2fc1812444be4cd49c178ba
hoodiedan <obodedaniel3@gmail.com>
2024-11-16 22:37:13 +0100

    app footer implemented

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,41 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 left">
+                    <img src="../assets/images/logo.svg" alt="logo">
+                    <h1>We Axxel at Service</h1>
+                    <p class="mt-4">
+                        Axxela is always looking for great partners. We would love to learn how we can help you
+                    </p>
+                    <a class="linkedin" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-md-6 right">
+                    <h6>Contact Us</h6>
+                    <p>
+                        Business owners with questions, please call Nate Goulbourne at (647) 227-9843 or email him at nate@herecia.partners
+                    </p>
+
+                    <form action="/">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <input type="text" placeholder="Enter Full Name">
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <input type="text" placeholder="Enter Your Email">
+                            </div>
+                        </div>
+
+                        <button class="w-100">Get in Touch</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>

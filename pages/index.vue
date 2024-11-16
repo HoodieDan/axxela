2b930f736e831f8d7bbb0b9e388e0a04e53ec0e7
hoodiedan <obodedaniel3@gmail.com>
2024-11-17 00:06:51 +0100

    hero section for home page implemented

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,70 @@
 <template>
-    <div>
-        <h1 class="maxima">Hello world</h1>
+    <div class="index__page">
+        <section class="hero__section">
+            <div class="bg">
+                <img src="../assets/images/hero-bg.png" alt="hero background">
+                <div class="hero__gradient"></div>
+            </div>
+
+            <div class="container">
+                <div class="heading">
+                    <div class="heading__content text-center">
+                        <h1>Canada’s Preferred HVACR Service Provider</h1>
+                        <h4>We Axxel at Service. A Reliable Partner You Can Count On.</h4>
+                        <button>JOIN THE FAMILY</button>
+                    </div>
+
+                    <div class="d-flex align-items-center justify-content-between w-100 mb-5 services">
+                        <h4>Chillers</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                        </svg>
+                        <h4>Boilers</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                        </svg>
+                        <h4>Cooling Towers</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                        </svg>
+                        <h4>HVAC</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                        </svg>
+                        <h4>Refridgeration</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                        </svg>
+                        <h4>Rooftop Units</h4>
+                    </div>
+
+                    <div class="row w-100 mt-1 features">
+                        <div class="col-lg-5 col-md-6 mt-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4>Acquisition Program</h4>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                                </svg>
+                            </div>
+                            <p>Scale your business while preserving your brand, legacy, and autonomy with Axxela</p>
+                        </div>
+                        <div class="col-lg-2 col-0"></div>
+                        <div class="col-lg-5 col-md-6 mt-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4>Join the Family</h4>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                                </svg>
+                            </div>
+                            <p>Interested in a partner committed to customer service, quality, and your team? Contact us today and lets gets the conversation started.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 <script>
-export default {
-    
-}
 </script>
 <style lang="">
     

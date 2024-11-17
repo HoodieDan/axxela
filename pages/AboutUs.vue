27f79c7d638253783f918c35ca2d1edb9cdfb3d5
hoodiedan <obodedaniel3@gmail.com>
2024-11-17 04:10:00 +0100

    about page implemented

--- pages/AboutUs.vue
+++ pages/AboutUs.vue
@@ -12,6 +12,164 @@
                 <p>We’re a growing network of HVAC and mechanical services companies in Canada</p>
             </div>
         </div>
+
+        <section class="who__we__are">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3>Who We Are: A Better Alternative</h3>
+                <p class="mt-4">
+                    Axxela Mechanical (Axxela) provides mission-critical heating, ventilation, air conditioning and refrigeration (HVACR) services to Commercial and Industrial clients across Canada.  Axxela was formed to provide a better alternative for HVACR contractors looking to sell their business.  We know it takes decades not years to build a successful business so work diligently to preserve your company’s legacy and scale it to greater heights.  Our tailored and decentralized approach provides access to shared resources and support services so our partners and employees can focus on what they love most —providing top-notch service.
+                </p>
+            </div>
+        </section>
+
+        <section class="difference">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3 class="mb-5">Why We Are Different: A People-First Approach”</h3>
+
+                <div class="quote__card mt-4">
+                    <p class="quote">Axxela is long-term and growth focused. We see Axxela as the premier HVACR company in Canada in a few years. We are excited to offer a better alternative for HVACR owners looking to sell than short-term and cost-cutting private equity or private-equity-backed companies.</p>
+                    <div class="row mt-4">
+                        <div class="col-1">
+                            <img src="../assets/images/ahmed.png" alt="co-founder image" class="img-fluid">
+                        </div>
+                        <div class="col-11 d-flex flex-column justify-content-around">
+                            <h6>Ahmed Alimi</h6>
+                            <p>Co-Founder of Axxela Mechanical</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="quote__card mt-4">
+                    <p class="quote">We know the hard work Commercial HVACR contractors put into serving their local communities. Importantly, we know it takes great employees to drive even greater customer experiences. We intend to keep it that way! Owners  prefer to partner with us because they know their employees and customers are in good hands.</p>
+                    <div class="row mt-4">
+                        <div class="col-1">
+                            <img src="../assets/images/nate.png" alt="co-founder image" class="img-fluid">
+                        </div>
+                        <div class="col-11 d-flex flex-column justify-content-around">
+                            <h6>Nate Goulbourne</h6>
+                            <p>Co-Founder of Axxela Mechanical</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="employees">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3 class="mb-5">What Your Employees Get</h3>
+
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 pe-lg-5 pe-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/growth.svg" alt="growth image">
+                            <h3 class="mt-4 mb-4">Growth Opportunities</h3>
+                            <p>We’re rapidly growing and investing in talent to create more promotions opportunities.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ps-lg-5 ps-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/stable.svg" alt="stable home image">
+                            <h3 class="mt-4 mb-4">Stable Home</h3>
+                            <p>We are committed to preserving customer relationships while retaining employees.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="owners">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3 class="mb-5">What Your Employees Get</h3>
+
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 pe-lg-5 pe-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/legacy.svg" alt="legacy preservation image">
+                            <h3 class="mt-4 mb-4">Legacy Preservation</h3>
+                            <p>We are proud of the company you have built. We keep 100% of the brand and employees.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ps-lg-5 ps-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/streamlined.svg" alt="streamlined process image">
+                            <h3 class="mt-4 mb-4">Streamlined Process</h3>
+                            <p>We move quickly and efficiently giving you an initial written proposal in 1-2 weeks</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 pe-lg-5 pe-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/transaction.svg" alt="transaction flexibility image">
+                            <h3 class="mt-4 mb-4">Transaction Flexibility</h3>
+                            <p>Our offer is tailored to you. Owners have the choice to sell 70-100% and stay on or retire</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ps-lg-5 ps-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/fair.svg" alt="fair valuation image">
+                            <h3 class="mt-4 mb-4">Fair Valuation</h3>
+                            <p>We pay market prices. We love to ensure you get the financial outcome you deserve. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="looking__for">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3 class="mb-5">What We Look For</h3>
+
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 pe-lg-5 pe-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/happy.svg" alt="happy customers image">
+                            <h3 class="mt-4 mb-4">Happy Customers</h3>
+                            <p>Clear commitment to quality demonstrated by repeat customers </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ps-lg-5 ps-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/passionate.svg" alt="passionate entrepreneurs image">
+                            <h3 class="mt-4 mb-4">Passionate Entrepreneurs</h3>
+                            <p>Independent and proud business builders.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 pe-lg-5 pe-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/team.svg" alt="team and culture image">
+                            <h3 class="mt-4 mb-4">Team and Culture</h3>
+                            <p>Happy, tenured employees dedicated to the future</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ps-lg-5 ps-md-5">
+                        <div class="deet__card">
+                            <img src="../assets/images/profitability.svg" alt="profitability image">
+                            <h3 class="mt-4 mb-4">Profitability</h3>
+                            <p>Wide range from $250K+ to multi-millions in annual profit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="process">
+            <div class="container">
+                <div class="h-bar"></div>
+                <h3 class="mt-4 mb-4">Our Process: A Unique Value Creation Opportunity</h3>
+                <p>We are HVAC people buying HVAC businesses so can move quickly and efficiently.  We’ve built an acquisition process around owners that respects your time and day-to-day operations. If you are interested in seeing an initial written proposal, we can provide one in 1-2 weeks with a 30-minutes call and a 2-3 years of financial statements. </p>
+
+                <h3 class="mt-4 mb-4">What Are The Next Steps To Partnering With Axxela?</h3>
+                <p class="d-block">1. Preliminary Conversation</p>
+                <p class="d-block">2. Non-Disclosure Agreement</p>
+                <p class="d-block">3. Share Financial Information</p>
+                <p class="d-block">4. Due Diligence/ Site Visit</p>
+                <p class="d-block">5. Close Deal And Transfer Funds</p>
+            </div>
+        </section>
     </div>
 </template>
 <script>

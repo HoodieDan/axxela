b8987ec5d6c42dcefaa944b7a0157def0cf3fae8
hoodiedan <obodedaniel3@gmail.com>
2024-11-17 02:39:00 +0100

    changed favicon and title

--- app.vue
+++ app.vue
@@ -10,6 +10,17 @@
 
 <script lang="ts" setup>
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+useHead({
+  title: 'Axxela Mechanical',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/logo.svg'
+    }
+  ]
+})
 </script>
 
 <style lang="scss">
